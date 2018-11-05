cb94e26f3928753717a6d0f914f9cdf3acb0ab67
vaarigupta <vaarigupta24@gmail.com>
2018-11-05 15:18:28 +0530

    *ngIf used

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,10 +11,13 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
    
     heroes = HEROES;
-    hero : Hero = {
-    id : 1,
-    name : 'WindStorm'
-    }
+ 
+
+    selectedHero: Hero;
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
   constructor() { 
   
   }
